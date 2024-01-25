3ce8608280fe3e5305eaf85b83e6cf16e9995de1
Göktuğ Gürler <goktugrlr@gmail.com>
2024-01-25 21:25:09 +0300

    Minimap Added

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform vehicle;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = vehicle.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, vehicle.eulerAngles.y, 0f);
+    }
+}
--- Assets/Scripts/TargetIndicator.cs
+++ Assets/Scripts/TargetIndicator.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 public class TargetIndicator : MonoBehaviour
 {
     public float rotationSpeed;
+    public Transform vehicle;
 
     private Transform target;
-
     void Update()
     {
-            target = GameObject.FindGameObjectWithTag("Target").transform;
+        target = GameObject.FindGameObjectWithTag("Target").transform;
 
-            Vector3 direction = target.position - transform.position;
-            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+        Vector3 direction = target.position - vehicle.position;
+        Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
 
-            toRotation *= Quaternion.Euler(0f, -90f, 0f);
+        toRotation *= Quaternion.Euler(0f, -90f, 0f);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+
     }
 }
